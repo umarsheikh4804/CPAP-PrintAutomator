5e5e4db7ba9338ae499181648a8db8d6c5ab8110
Umar Sheikh <93892730+umarsheikh4804@users.noreply.github.com>
2021-11-29 07:33:12 -0600

    Update print_auto.py

--- print_auto.py
+++ print_auto.py
@@ -1,20 +1,22 @@
 import sys
+import time
+
+import pyautogui
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-import pyautogui
-import time
-from datetime import date
 
 # executable_path=r'C:/Users/Umar Sheikh/Downloads/chromedriver_win32/chromedriver.exe''
 
 login_info = {
-              'rm1' : ['https://login.airview.resmed.com/custom-login?response_type=code&client_id=0oa7ca7b9yNqH8sBI297&scope=openid%20email%20profile&state=yrebdRlYyAvp8n5YaaTqpZ24FE-uypYOqMyKmcLHaJA%3D&redirect_uri=https://login.airview.resmed.com/authorization-code/callback&nonce=sgJCAiO33lvMlHuD_0EzFdauqKmEUD00ArPkav9ppEI',
-                    'cpapdoc', 'Mpsc_2015'],
-              'rm2' : ['https://login.airview.resmed.com/custom-login?response_type=code&client_id=0oa7ca7b9yNqH8sBI297&scope=openid%20email%20profile&state=yrebdRlYyAvp8n5YaaTqpZ24FE-uypYOqMyKmcLHaJA%3D&redirect_uri=https://login.airview.resmed.com/authorization-code/callback&nonce=sgJCAiO33lvMlHuD_0EzFdauqKmEUD00ArPkav9ppEI',
-                    'metroplex2', 'Mpsc_2021'],
-              'co' : ['https://www.careorchestrator.com/#/login',
-                    'jmodesto', 'Mpsc2020!']
+    'rm1': [
+        'https://login.airview.resmed.com/custom-login?response_type=code&client_id=0oa7ca7b9yNqH8sBI297&scope=openid%20email%20profile&state=yrebdRlYyAvp8n5YaaTqpZ24FE-uypYOqMyKmcLHaJA%3D&redirect_uri=https://login.airview.resmed.com/authorization-code/callback&nonce=sgJCAiO33lvMlHuD_0EzFdauqKmEUD00ArPkav9ppEI',
+        '***', '***'],
+    'rm2': [
+        'https://login.airview.resmed.com/custom-login?response_type=code&client_id=0oa7ca7b9yNqH8sBI297&scope=openid%20email%20profile&state=yrebdRlYyAvp8n5YaaTqpZ24FE-uypYOqMyKmcLHaJA%3D&redirect_uri=https://login.airview.resmed.com/authorization-code/callback&nonce=sgJCAiO33lvMlHuD_0EzFdauqKmEUD00ArPkav9ppEI',
+        '***', '***'],
+    'co': ['https://www.careorchestrator.com/#/login',
+           '***', '***']
 }
 
 
@@ -27,18 +29,10 @@ def get_info():
         patient = appointment.split()[3:5]
         temp.append(patient)
     temp.remove(temp[0])
-    for name in temp:
-        str = name[1] + ' ' + name[0].replace(',', '')
+    for name in temp[:-1]:
+        str = name[1].lower() + ' ' + name[0].replace(',', '').lower()
         patient_names.append(str)
     return patient_names
-
-
-def check_element_exists_by_id(id):
-    try:
-        driver.find_element_by_id(id)
-    except NoSuchElementException:
-        return False
-    return True
 
 
 def check_element_exists_by_css_selector(id):
@@ -69,15 +63,16 @@ def check_rm(num_rm):
         search_bar.send_keys(patient)
         search_bar.submit()
 
-        if not check_element_exists_by_id('empty-patient-label'):
-            # make the following step a bit faster
+        if check_element_exists_by_css_selector('a.nameAsLink'):
             patient_code = driver.find_element_by_class_name('patient-row').get_attribute('id')
-            cpap_report_url = 'https://airview.resmed.com/patients/{}/report/complianceandtherapy/Compliance_and_therapy_report_06292021_183334.pdf?returningUrl=%2Fpatients%2F{}%2Fcharts&reportType=ComplianceAndTherapy&reportPeriodType=SUPPLIED&reportingPeriodLength=90&reportingPeriodStart=undefined&reportingPeriodEnd=06%2F28%2F2021&nightProfileDays=7'.format(patient_code, patient_code)
+            cpap_report_url = 'https://airview.resmed.com/patients/{}/report/complianceandtherapy/Compliance_and_therapy_report_06292021_183334.pdf?returningUrl=%2Fpatients%2F{}%2Fcharts&reportType=ComplianceAndTherapy&reportPeriodType=SUPPLIED&reportingPeriodLength=90&reportingPeriodStart=undefined&reportingPeriodEnd=06%2F28%2F2021&nightProfileDays=7'.format(
+                patient_code, patient_code)
             driver.get(cpap_report_url)
-            time.sleep(2)
+            time.sleep(1)
             pyautogui.hotkey('ctrl', 'p')
-            time.sleep(2)
+            time.sleep(3)
             pyautogui.press('Enter')
+            time.sleep(1)
             driver.execute_script("window.history.go(-1)")
             patient_list.remove(patient)
 
@@ -105,19 +100,23 @@ def check_co():
             first_patient_on_page = driver.find_element_by_css_selector('h3.pr-result-title.pr-result-title-link')
             if first_patient_on_page.text.lower() in patient_list:
                 first_patient_on_page.click()
-                time.sleep(2)
+                driver.find_element_by_link_text('Therapy data').click()
+                cpap_report_url = driver.find_element_by_tag_name('pr-pdf-viewer').get_attribute('src')
+                driver.get(cpap_report_url)
+                time.sleep(1)
                 pyautogui.hotkey('ctrl', 'p')
-                time.sleep(2)
+                time.sleep(3)
                 pyautogui.press('Enter')
-                driver.execute_script("window.history.go(-1)")
+                time.sleep(1)
+                driver.get('https://www.careorchestrator.com/#/patient/search')
 
     driver.close()
 
 
 patient_list = get_info()
 driver = webdriver.Chrome(executable_path=r'C:/Users/Umar Sheikh/Downloads/chromedriver_win32/chromedriver.exe')
-driver.maximize_window() # For maximizing window
-driver.implicitly_wait(10) # gives an implicit wait for 10 seconds
+driver.maximize_window()  # For maximizing window
+driver.implicitly_wait(10)  # gives an implicit wait for 10 seconds
 check_rm(1)
 check_rm(2)
 check_co()
